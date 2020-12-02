044caa5ab52dd32357ad8e6791ddc9e6af143572
shenshan <shenshanpku@gmail.com>
2020-12-02 09:59:17 -0800

    remove colony name

--- hillman_mouse_pipeline/+subject/Subject.m
+++ hillman_mouse_pipeline/+subject/Subject.m
@@ -1,18 +1,18 @@
 
 %{
 # Subject
-subject_id: varchar(32)                  # unique mouse id
+subject_id                  : varchar(32)                   # unique mouse id
 ---
-dob: date                     # mouse date of birth
-sex='U': enum('M', 'F', 'U')       # sex of mouse - Male, Female, or Unknown/Unclassified
-breeding_pair='':    varchar(255)             # 2 str for parents
+dob                         : date                          # mouse date of birth
+sex="U"                     : enum('M','F','U')             # sex of mouse - Male, Female, or Unknown/Unclassified
+breeding_pair               : varchar(255)                  # 2 str for parents
 -> subject.Species
-mark_type='ear': enum('ear', 'toe', 'tail')
-mark='':             varchar(128)
+mark_type="ear"             : enum('ear','toe','tail')      # 
+mark                        : varchar(128)                  # 
 -> lab.Source
 -> subject.Strain
-subject_notes='':    varchar(1024)
-subject_ts = CURRENT_TIMESTAMP: timestamp
+subject_notes               : varchar(1024)                 # 
+subject_ts="current_timestamp()": timestamp                 # 
 %}
 
 classdef Subject < dj.Manual
