ae088779d7d7d0a629318de90a3c0ad587c8a262
ajy2120 <58821707+ajy2120@users.noreply.github.com>
2021-01-27 16:06:10 -0500

    re-add scape python code

---
+++ hillman_pipeline/lab.py
@@ -0,0 +1,58 @@
+import datajoint as dj
+
+schema = dj.schema('hillman_lab')
+
+
+@schema
+class Lab(dj.Lookup):
+    definition = """
+    lab                     : varchar(32)    # e.g. Randylab, Hillmanlab
+    ---
+    lab_description=''      : varchar(255)
+    """
+    contents = [['hillmanlab', '']]
+
+
+@schema
+class LabMember(dj.Lookup):
+    definition = """
+    user        : varchar(32)
+    ---
+    -> Lab
+    """
+    contents = [['richard yan','hillmanlab'],
+                ['wenze li', 'hillmanlab'],
+                ['hang yu','hillmanlab'],
+                ['wenxuan liang','hillmanlab'],
+                ['sam benezra','hillmanlab'],
+                ['malte casper','hillmanlab'],
+                ['grace lee','hillmanlab'],
+                ['kripa patel','hillmanlab'],
+                ['citlali campos','hillmanlab'],
+                ['venkata voleti','hillmanlab'],
+                ['boss','hillmanlab']]
+
+
+@schema
+class Source(dj.Lookup):
+    definition = """
+    source_name:    varchar(32)               # unique source id
+    ---
+    source_full_name='': varchar(255)  	      # name of source
+    contact_details='': varchar(255)
+    link_to_wedsite='':  varchar(255)
+    source_description='':	varchar(255)
+    source_ts = CURRENT_TIMESTAMP: timestamp
+    """
+
+
+@schema
+class Project(dj.Lookup):
+    definition = """
+    project                     : varchar(32)
+    ---
+    -> LabMember
+    project_description=''      : varchar(1024)
+    """
+    contents =[['NeuroPal','richard yan',''],
+               ['KimaraHeart','richard yan','']]
