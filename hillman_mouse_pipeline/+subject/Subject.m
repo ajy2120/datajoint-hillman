3a56d617d1b8cdd5cf89ffee330d3ad8a42cc09d
ajy2120 <58821707+ajy2120@users.noreply.github.com>
2021-03-25 15:27:52 -0400

    add StrainAllele and troubleshooting subject table

--- hillman_mouse_pipeline/+subject/Subject.m
+++ hillman_mouse_pipeline/+subject/Subject.m
@@ -5,7 +5,7 @@ subject_id                  : varchar(32)                   # unique mouse id
 ---
 dob                         : date                          # mouse date of birth
 sex="U"                     : enum('M','F','U')             # sex of mouse - Male, Female, or Unknown/Unclassified
-breeding_pair               : varchar(255)                  # 2 str for parents
+breeding_pair=''            : varchar(255)                  # 2 str for parents
 -> subject.Species
 mark_type="ear"             : enum('ear','toe','tail')      # 
 mark                        : varchar(128)                  # 
